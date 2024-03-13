543dd6055ea9aed7d7faebcac6f8201f3653c663
Nafikovtt <nafikovtt@gmail.com>
2024-03-13 17:52:53 +0500

    little fix

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,2 +1,46 @@
 package ru.netology
 
+data class Post(
+    val id: Int,
+    val ownerId: Int,
+    val createdBy: Int = 5,
+    val date: Int,
+    val text: String,
+    val comments: Comments = Comments(),
+    val canPin: Boolean = false,
+    val canDelete: Boolean = false,
+    val canEdit: Boolean = false,
+    val postType: String = "post",
+)
+
+data class Comments(
+    val count: Int = 0,
+    val canPost: Boolean = false,
+)
+
+object WallService {
+
+    private var nextId = 1
+    private var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        val newPost = post.copy(id = nextId++)
+        posts += newPost
+        return newPost
+    }
+
+    fun update(post: Post): Boolean {
+        val index = posts.indexOfFirst { it.id == post.id }
+        if (index != -1) {
+            posts = posts.copyOf()
+            posts[index] = post
+            return true
+        }
+        return false
+    }
+
+    fun clear() {
+        posts = emptyArray()
+        nextId = 1
+    }
+}
--- src/test/kotlin/ru/netology/wallServiceTest.kt
+++ src/test/kotlin/ru/netology/wallServiceTest.kt
@@ -3,14 +3,35 @@ package ru.netology
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class wallServiceTest {
 
-    @Test
-    fun add() {
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
     }
 
     @Test
-    fun update() {
+    fun addPostNotZero() {
+        val post = Post(1, 1, 5, 123123123, "Test post")
+        val addedPost = WallService.add(post)
+        assertTrue(addedPost.id != 0)
+    }
+
+    @Test
+    fun updateExistingPostTrue() {
+        val post = Post(1, 1, 5, 123123, "Test post")
+        WallService.add(post)
+        val updatedPost = post.copy(text = "Updated test post")
+        val result = WallService.update(updatedPost)
+        assertTrue(result)
+    }
+
+    @Test
+    fun updateNonExistingPostFalse() {
+        val post = Post(1, 1, 5, 123141, "Test post")
+        val result = WallService.update(post)
+        assertFalse(result)
     }
 }
