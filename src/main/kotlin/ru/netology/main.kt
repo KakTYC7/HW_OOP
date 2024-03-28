51ea9ad68a30d940f35887da3173defa3935d04b
Nafikovtt <nafikovtt@gmail.com>
2024-03-28 15:42:06 +0500

    HW_OOP 2.3

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,17 +1,93 @@
 package ru.netology
 
+import java.util.*
+import kotlin.contracts.CallsInPlace
+
 data class Post(
     val id: Int,
     val ownerId: Int,
     val createdBy: Int = 5,
     val date: Int,
     val text: String,
-    val comments: Comments = Comments(),
-    val canPin: Boolean = false,
-    val canDelete: Boolean = false,
+    val comments: Comments? = null,
+    val canPin: Boolean? = null,
+    val canDelete: Boolean? = null,
     val canEdit: Boolean = false,
     val postType: String = "post",
+    val attachments: Array<Attachment>? = null,
 )
+
+interface Attachment {
+    val type: String
+}
+
+data class PhotoAttachment(
+    val photo: Photo
+) : Attachment {
+    override val type: String = "photo"
+}
+
+data class VideoAttachment(
+    val video: Video
+) : Attachment {
+    override val type: String = "video"
+}
+
+data class AudioAttachment(
+    val video: Audio
+) : Attachment {
+    override val type: String = "audio"
+}
+
+data class FileAttachment(
+    val video: File
+) : Attachment {
+    override val type: String = "file"
+}
+
+data class GeoAttachment(
+    val video: Geo
+) : Attachment {
+    override val type: String = "geo"
+}
+
+data class Photo(
+    val id: Int,
+    val ownerId: Int,
+    val userId: Int,
+    val text: String,
+)
+
+data class Video(
+    val id: Int,
+    val ownerId: Int,
+    val title: String,
+    val duration: Int
+)
+
+data class Audio(
+    val id: Int,
+    val ownerId: Int,
+    val artist: String,
+    val duration: Int,
+    val url: String
+)
+
+data class File(
+    val id: Int,
+    val ownerId: Int,
+    val title: String,
+    val size: Int,
+    val url: String
+)
+
+data class Geo(
+    val type: String,
+    val coordinates: Objects,
+    val place: Objects
+)
+
+
 
 data class Comments(
     val count: Int = 0,
